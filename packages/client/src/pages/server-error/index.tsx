77252b58c82c59ef8ec881f57d5d0f9ff33204ec
albinasaveleva <savelevalbina@gmail.com>
2024-12-19 15:14:50 +0300

    add ServerErrorPage

---
+++ packages/client/src/pages/server-error/index.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+import ErrorPage from '../../components/error-page';
+
+const ServerErrorPage: FC = () => {
+  return (
+    <>
+      <ErrorPage id={"server-error-page"} title={"500"} subtitle={"Oops, that's our bad."} />
+    </>
+  )
+}
+
+export default ServerErrorPage;
