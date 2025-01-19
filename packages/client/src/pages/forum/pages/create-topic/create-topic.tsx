206c5aa69862f9a40d41d4096c2a999a000b805b
Korno <korno9612@gmail.com>
2025-01-20 00:54:07 +0200

    add Form rules

--- packages/client/src/pages/forum/pages/create-topic/create-topic.tsx
+++ packages/client/src/pages/forum/pages/create-topic/create-topic.tsx
@@ -1,24 +1,18 @@
 import { Form, Input, Button, Typography, Flex } from "antd"
+import { getFormRules } from "../../../../components/form-rules"
 
 const CreateTopic = () => {
+  const { requiredFieldRule } = getFormRules()
   return (
     <Flex vertical align={"center"} gap={24}>
       <Typography.Title level={3}>New Topic</Typography.Title>
 
       <Form style={{ width: "60%" }} layout="vertical">
-        <Form.Item
-          label="Title"
-          name="title"
-          rules={[{ required: true, message: "Please enter the post title!" }]}>
+        <Form.Item label="Title" name="title" rules={[requiredFieldRule]}>
           <Input />
         </Form.Item>
 
-        <Form.Item
-          label="Content"
-          name="content"
-          rules={[
-            { required: true, message: "Please enter the post content!" },
-          ]}>
+        <Form.Item label="Content" name="content" rules={[requiredFieldRule]}>
           <Input.TextArea rows={5} />
         </Form.Item>
 
