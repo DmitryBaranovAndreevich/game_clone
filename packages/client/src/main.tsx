3a701886f09124b9d3f627f0b9732748361fbdf6
Korno <korno9612@gmail.com>
2025-01-21 03:31:56 +0200

    add errorBoundary

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,13 +5,16 @@ import App from "./app/App"
 import { registerServiceWorker } from "./utils/serviceWorker"
 import "./index.css"
 import store from "./store"
+import ErrorBoundary from "./components/error-boundary"
 
 registerServiceWorker()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ErrorBoundary fallback={<h1 style={{ color: "white" }}>error</h1>}>
+        <App />
+      </ErrorBoundary>
     </Provider>
     ,
   </React.StrictMode>,
