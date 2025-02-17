57df9d39bd83ef65e19ffdfd1b3fbedae9a7eae5
zinchenkoaa <zinchenkoa2@yandex.ru>
2025-02-17 18:41:36 +0300

    настроил redux и router в ssr

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,14 +1,18 @@
 import { AntdConfigProvider } from "./antd-config-provider"
 import React from "react"
-import store from "../store"
 import ErrorBoundary from "../components/error-boundary"
 import { Provider } from "react-redux"
 import { createCache, StyleProvider } from "@ant-design/cssinjs"
 import AppRoutes from "./app-routes"
+import { makeStore } from "../store"
 
 export const cache = createCache()
 
 function App() {
+  // @ts-ignore
+  const preloadedState = (window as never).APP_INITIAL_STATE || "{}"
+  const store = makeStore(preloadedState)
+
   return (
     <React.StrictMode>
       <StyleProvider cache={cache}>
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,12 +3,23 @@ import App from "./app/App"
 import { registerServiceWorker } from "./utils/serviceWorker"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { makeStore } from "./store"
 
 registerServiceWorker()
 
+// 🟢 Получаем предзагруженное состояние Redux из глобального объекта
+// @ts-ignore
+const preloadedState = (window as never).APP_INITIAL_STATE || "{}"
+
+// 🔵 Десериализуем состояние (с проверкой на безопасность)
+const store = makeStore(preloadedState)
+
 ReactDOM.hydrateRoot(
   document.getElementById("root") as HTMLElement,
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 )
