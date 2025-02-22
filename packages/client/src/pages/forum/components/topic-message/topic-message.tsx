d9718a8191b8a1a383fab1d98e4f236c7993bcad
Korno <korno9612@gmail.com>
2025-02-22 22:57:42 +0200

    add emoji picker

--- packages/client/src/pages/forum/components/topic-message/topic-message.tsx
+++ packages/client/src/pages/forum/components/topic-message/topic-message.tsx
@@ -1,5 +1,8 @@
 import { Avatar, Card, Typography } from "antd"
 import styles from "./topic-message.module.css"
+import EmojiPicker from "emoji-picker-react"
+import { useEffect, useRef, useState } from "react"
+import { ReactionIcon } from "../../../../assets/images/icons/reactionIcon"
 
 interface Message {
   name: string
@@ -8,6 +11,31 @@ interface Message {
 }
 
 const TopicMessage: React.FC<{ message: Message }> = ({ message }) => {
+  const [openEmojiPicker, setOpenEmojiPicker] = useState(false)
+  const pickerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        pickerRef.current &&
+        !pickerRef.current.contains(event.target as Node)
+      ) {
+        setOpenEmojiPicker(false)
+      }
+    }
+    if (openEmojiPicker) {
+      document.addEventListener("mousedown", handleClickOutside)
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [openEmojiPicker])
+
+  const hendalReaction = () => {
+    setOpenEmojiPicker(false)
+    console.log("UpdateReaction")
+  }
+
   return (
     <Card className={`${styles[message.type]}`}>
       <Avatar size={40} className={styles.avatar}>
@@ -15,6 +43,26 @@ const TopicMessage: React.FC<{ message: Message }> = ({ message }) => {
       </Avatar>
       <Typography.Text>{message.name}</Typography.Text>
       <p>{message.content}</p>
+
+      <span style={{ float: "right" }}>
+        {!openEmojiPicker && (
+          <span onClick={() => setOpenEmojiPicker(true)}>
+            <ReactionIcon width="25px" height="25px" />
+          </span>
+        )}
+
+        {openEmojiPicker && (
+          <div ref={pickerRef}>
+            <EmojiPicker
+              reactionsDefaultOpen={true}
+              allowExpandReactions={false}
+              onReactionClick={hendalReaction}
+              theme={"dark"}
+              skinTonesDisabled={true}
+            />
+          </div>
+        )}
+      </span>
     </Card>
   )
 }
